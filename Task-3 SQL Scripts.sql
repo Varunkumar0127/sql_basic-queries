c8b433107e8b36166bd4af2ee7753549f2361db0
Varunkumar <varunkumarselvam@gmail.com>
2025-08-11 23:16:28 +0530

    Add files via upload

---
+++ Task-3 SQL Scripts.sql
@@ -0,0 +1,18 @@
+select *from guests;
+select *from rooms;
+select *from bookings;
+select *from payments;
+select *from services_used;
+select *from rooms where RoomID is not null;
+select *from rooms where RoomType='Suite' and PricePerNight=5000;
+select *from payments where PaymentMethod='UPI' or Amountpaid='3000';
+select *from guests where Name like '%a';
+select *from guests where Phone like '%0';
+select *from payments where AmountPaid between 5000 and 8000;
+select *from bookings where CheckInDate between '2025-08-10' and '2025-08-15';
+select *from services_used where ServiceID not between 1 and 3;
+select Name,Email from guests order by Name;
+select *from payments order by Amountpaid desc;
+select *from rooms order by RoomType;
+select *from services_used order by ServiceDate desc;
+
